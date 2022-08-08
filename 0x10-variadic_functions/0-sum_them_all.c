89e080cb73809e595e0a9fb6be505fc5dabb743f
Monisola <you@example.com>
2022-08-08 16:00:10 -0700

    Learning variadic function in C

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,19 +10,19 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list args;
+	va_list valist;
 	unsigned int i;
 	int sum = 0;
 
 	if (n == 0)
 		return (0);
 
-	va_start(args, n);
+	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_args(args, int);
+		sum += va_args(valist, int);
 
-	va_end(args);
+	va_end(valist);
 
 	return (sum);
 }
