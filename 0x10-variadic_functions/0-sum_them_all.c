ce3a6ec6e7821451c8871b0981db22c146fc82be
Monisola <you@example.com>
2022-08-08 16:46:29 -0700

    Learning variadic functions in C

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,9 +2,9 @@
 
 /**
  * sum_them_all - returns the sum of all its parameters.
- * @n: amount of all arguments
+ * @n: amount of the arguments.
  *
- * Return: sum of its parameters
+ * Return: sum of its parameters.
  */
 int sum_them_all(const unsigned int n, ...)
 {
@@ -18,7 +18,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_args(valist, int);
+		sum += va_arg(valist, int);
 
 	va_end(valist);
 
